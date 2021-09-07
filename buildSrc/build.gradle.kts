feaf06078ce5da010140f31aefa4f817ec8568b5
TheBestPessimist <cristian@tbp.land>
2021-09-07 14:18:03 +0300

    Add spring dependency Add question about plugin dependencies and names

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,10 @@
+// TODO: how can i not duplicate V?
+object V {
+    const val springBootVersion = "2.5.3"
+    const val kotlinVersion = "1.5.10"
+}
+
+
 plugins {
     `kotlin-dsl`
 }
@@ -10,7 +17,15 @@ dependencies {
     implementation("gradle.plugin.com.github.spotbugs.snom:spotbugs-gradle-plugin:4.6.2")
     testImplementation("junit:junit:4.13")
 
-    // used for kotlin with spring
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+    // used for kotlin with spring project
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${V.kotlinVersion}")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:${V.springBootVersion}")
+
+//    TODO: what is the name of the plugin containing kotlin spring ( kotlin("plugin.spring") )?
+//      From https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.spring, i expect it to be
+//      implementation("org.jetbrains.kotlin:kotlin-allopen:1.5.10")
+//      but that makes no sense.
+//      @gradle documentation folks: Where can i find the name of the plugins??
+
 
 }
--- buildSrc/src/main/kotlin/kotlin-with-spring.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-with-spring.gradle.kts
@@ -1,3 +1,34 @@
-plugins{
+// TODO: how can i not duplicate V?
+object V {
+    const val springBootVersion = "2.5.3"
+    const val kotlinVersion = "1.5.10"
+}
+
+plugins {
     kotlin("jvm")
+    id("org.springframework.boot")
+//    TODO: what is the name of this plugin? how to import it? see the question inside the dependencies
+//    kotlin("plugin.spring")
+
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:${V.springBootVersion}"))
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.integration:spring-integration-core")
+    developmentOnly("org.springframework.boot:spring-boot-devtools:${V.springBootVersion}")
+
+
+}
+
+
+repositories {
+    mavenCentral()
+
 }
